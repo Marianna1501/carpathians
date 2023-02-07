8aa01681119afe09e4cbf185373f3f8ec0a84d94
Marianna1501 <109352232+Marianna1501@users.noreply.github.com>
2023-02-07 14:01:41 +0000

    Deploying to gh-pages from @ Marianna1501/carpathians@2b2827c3f2f865d2296c062b97838318cdbd7b1e 🚀

---
+++ index.23cfb922.js
@@ -0,0 +1,2 @@
+!function(){var e,t;e=document.querySelector("[data-menu-button]"),t=document.querySelector("[data-menu]"),e.addEventListener("click",(function(){e.classList.toggle("is-open"),t.classList.toggle("is-open")}))}();
+//# sourceMappingURL=index.23cfb922.js.map
---
+++ index.9f5e24f1.js
@@ -0,0 +1,2 @@
+(()=>{const e=document.querySelector("[data-menu-button]"),t=document.querySelector("[data-menu]");e.addEventListener("click",(()=>{e.classList.toggle("is-open"),t.classList.toggle("is-open")}))})();
+//# sourceMappingURL=index.9f5e24f1.js.map
